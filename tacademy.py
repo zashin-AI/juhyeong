864e9a165301d12a2330732c26f2f92f9ad33471
skanwngud <skanwngud@gmail.com>
2021-04-01 20:50:04 +0900

    commit

--- tacademy.py
+++ tacademy.py
@@ -1,3 +1,13 @@
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.premade import 
+
+import librosa
+
+audio, sr = librosa.load(
+    'c:/data/sushi2.wav'
+)
+
+print(audio)
+print(len(audio)) # 304820
+print(sr) # 22050
+print(len(audio)/sr) # 13 sec
