f9ead52a01bd61b686dd9bb6f35ffbfd076d4df5
skanwngud <skanwngud@gmail.com>
2021-04-02 14:30:55 +0900

    commit

--- pansori.py
+++ pansori.py
@@ -1,8 +1,9 @@
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, LSTM, Conv1D, MaxPool1D
 from tensorflow.keras.models import Sequential
 
 import librosa
 import librosa.display
+import soundfile as sf
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,12 +11,18 @@ y, sr = librosa.load(
     'c:/nmb/nmb_data/testvoice_주형.wav'
 )
 
-S = librosa.feature.melspectrogram(y, sr=sr)
-S_DB = librosa.amplitude_to_db(S, ref = np.max)
+print(len(y))
+print(len(y)/sr)
 
-plt.figure(figsize = (16, 6))
-librosa.display.specshow(
-    S_DB, x_axis = 'time', y_axis = 'log', sr = sr
-)
-plt.colorbar()
-plt.show()
+length = int(len(y)/sr)
+sec = length * sr
+
+print(length)
+print(sec)
+
+y2 = y[:sec]
+# y2 = y[:round(len(y)/sr)]
+
+print(len(y2))
+
+sf.write('c:/nmb/nmb_data/주형.wav', y2, sr)
