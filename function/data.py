f7477fbffbb9ad005779a8d590770ed6a9fe9b47
skanwngud <skanwngud@gmail.com>
2021-04-07 17:57:21 +0900

    commit

--- function/data.py
+++ function/data.py
@@ -5,7 +5,7 @@ filepath = 'C:/nmb/nmb_data/ForM/M/' # 파일 경로
 filename = 'flac' # 파일 확장자
 labels = 1 # 라벨값 지정
 
-dataset, label = datasave.load_data(filepath, filename, labels)
+dataset, label = datasave.load_data_mfcc(filepath, filename, labels)
 
 print(dataset)
 print(label)
--- function/datasave.py
+++ function/datasave.py
@@ -2,7 +2,7 @@ import librosa
 import numpy as np
 import sklearn
 
-def load_data(filepath, filename, labels):
+def load_data_mfcc(filepath, filename, labels):
 
     '''
     Args :
@@ -27,6 +27,34 @@ def load_data(filepath, filename, labels):
             mels = librosa.feature.mfcc(y, sr=sr)
             mels = librosa.amplitude_to_db(mels, ref=np.max)
             mels = normalize(mels, axis = 1)
+
+            dataset.append(mels)
+            label.append(labels)
+            print(str(count))
+            
+            count+=1
+
+    return dataset, label
+
+def load_data_mel(filepath, filename, labels):
+
+    count = 1
+
+    dataset = list()
+    label = list()
+
+    def normalize(x, axis=0):
+        return sklearn.preprocessing.minmax_scale(x, axis=axis)
+
+    files = librosa.util.find_files(filepath, ext=[filename])
+    files = np.asarray(files)
+    for file in files:
+        y, sr = librosa.load(file, sr=22050, duration=5.0)
+        length = (len(y) / sr)
+        if length < 5.0 : pass
+        else:
+            mels = librosa.feature.melspectrogram(y, sr=sr, n_fft=512, hop_length=128)
+            mels = librosa.amplitude_to_db(mels, ref=np.max)
 
             dataset.append(mels)
             label.append(labels)
