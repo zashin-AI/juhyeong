79c8b8c15ab404a7b50a93d49146432e038ac2bb
skanwngud <skanwngud@gmail.com>
2021-04-07 17:35:40 +0900

    commit

--- function/data.py
+++ function/data.py
@@ -1,10 +1,11 @@
 import datasave
 import numpy as np
 
-filepath = 'C:/nmb/nmb_data/ForM/M/'
-filename = 'flac'
+filepath = 'C:/nmb/nmb_data/ForM/M/' # 파일 경로
+filename = 'flac' # 파일 확장자
+labels = 1 # 라벨값 지정
 
-dataset, label = datasave.load_data(filepath, filename, 1)
+dataset, label = datasave.load_data(filepath, filename, labels)
 
 print(dataset)
 print(label)
