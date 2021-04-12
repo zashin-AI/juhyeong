8b280a2f9309c993d37eb4c97989fa168b97e0cb
skanwngud <skanwngud@gmail.com>
2021-04-12 11:03:51 +0900

    commit

--- denoise/tim/denoise2.py
+++ denoise/tim/denoise2.py
@@ -81,39 +81,21 @@ def removeNoise(
     visual = False
 ):
 
-    # Args:
-    #     audio_clip (array): The first parameter. / 기존 음성
-    #     noise_clip (array): The second parameter. / 노이즈 음성
-    #     n_grad_freq (int): how many frequency channels to smooth over with the mask. / mask 로 처리 할 주파수 채널의 수
-    #     n_grad_time (int): how many time channels to smooth over with the mask. / mask 로 부드럽게 처리 할 시간 채널의 수
-    #     n_fft (int): number audio of frames between STFT columns. / stft 컬럼을 몇 개로 자를 것인가
-    #     win_length (int): Each frame of audio is windowed by `window()`. The window will be of length `win_length` and then padded with zeros to match `n_fft`.. / window 사이즈
-    #     hop_length (int):number audio of frames between STFT columns. / stft 를 자를 때 얼마만큼 겹칠 것인가
-    #     n_std_thresh (int): how many standard deviations louder than the mean dB of the noise (at each frequency level) to be considered signal / 신호로 간주 할 잡음 수
-    #     prop_decrease (float): To what extent should you decrease noise (1 = all, 0 = none) / denoise 를 얼마만큼 실행시킬 것인가
-    #     visual (bool): Whether to plot the steps of the algorithm / 시각화 관련
+    noise_stft = stft(noise_clip, n_fft, hop_length, win_length)
+    noise_stft_db = amp_to_db(np.abs(noise_stft))
+    mean_freq_noise = np.mean(noise_stft_db, axis = 1)
+    std_freq_noise = np.std(noise_stft_db, axis = 1)
+    noise_thresh = mean_freq_noise + std_freq_noise * n_std_thresh
 
-    # if verbose:
-    noise_stft = stft(noise_clip, n_fft, hop_length, win_length) # noise file 를 받아 stft 화 시킴
-    noise_stft_db = amp_to_db(np.abs(noise_stft)) # stft 를 dB 로 바꿔줌
-    mean_freq_noise = np.mean(noise_stft_db, axis = 1) # stft 된 noise 의 평균
-    std_freq_noise = np.std(noise_stft_db, axis = 1) #  stft 된 noise 의 표준편차
-    noise_thresh = mean_freq_noise + std_freq_noise * n_std_thresh # noise stft 의 표준푠차와 n_std_thresh 를 곱한 값에 평균을 더함
-    print('noise_stft pass')
+    sig_stft = stft(audio_clip, n_fft, hop_length, win_length)
+    sig_stft_db = amp_to_db(np.abs(sig_stft))
 
-    # if verbose:
-    sig_stft = stft(audio_clip, n_fft, hop_length, win_length) # 원본 파일을 받아 stft 화 시킴
-    sig_stft_db = amp_to_db(np.abs(sig_stft)) # stft 를 dB 로 바꿔줌
-    print('sig_stft pass')
+    mask_gain_dB = np.min(amp_to_db(np.abs(sig_stft)))
 
-    mask_gain_dB = np.min(amp_to_db(np.abs(sig_stft))) # 원본 파일 stft 의 데이터를 dB 한 값의 최소값을 반환
-
-    smoothing_filter = np.outer( # 두 행렬의 곱. 여기선 각각 np.concatenate 들이다.
-        np.concatenate( # 1차원 배열들을 concat 함
+    smoothing_filter = np.outer(
+        np.concatenate(
             [ 
                 np.linspace(0, 1, n_grad_freq + 1, endpoint = False),
-                # linspace 함수를 이용하여 0 부터 1 까지 n_grad_freq + 1 (==3) 개의 1차원 배열을 생성
-                # endpoint = False 이므로 1 은 제외
                 np.linspace(1, 0, n_grad_freq + 2),
             ]
         )[1:-1],
@@ -129,7 +111,7 @@ def removeNoise(
         np.reshape(noise_thresh, [1, len(mean_freq_noise)]),
         np.shape(sig_stft_db)[1],
         axis = 0,
-    ).T # .T == np.transpose()
+    ).T
     
     sig_mask = sig_stft_db < db_thresh
     sig_mask = scipy.signal.fftconvolve(
@@ -137,7 +119,6 @@ def removeNoise(
     )
     sig_mask = sig_mask * prop_decrease
 
-    # if verbose:
     sig_stft_db_masked = (
         sig_stft_db * (1 - sig_mask)
         + np.ones(np.shape(mask_gain_dB)) * mask_gain_dB * sig_mask
@@ -146,16 +127,11 @@ def removeNoise(
     sig_stft_amp = (db_to_amp(sig_stft_db_masked * np.sign(sig_stft)) + (
         1j * sig_imag_masked
     ))
-    print('sig_stft_db pass')
     
-    # if verbose:
     recoverd_signal = istft(sig_stft_amp, hop_length, win_length)
     recoverd_spec = amp_to_db(
         np.abs(stft(recoverd_signal, n_fft, hop_length, win_length))
     )
-    print('recoverd_signal pass')
-    # if verbose:
-    print("finish")
     return recoverd_signal
 
 output = removeNoise(
