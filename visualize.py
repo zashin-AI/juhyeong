4a8fb4645f619ed7f4d321bfca0f1e58f3097b6a
skanwngud <skanwngud@gmail.com>
2021-04-21 18:04:40 +0900

    commit

--- visualize.py
+++ visualize.py
@@ -22,26 +22,52 @@ gan4, sr = librosa.load(
     'c:/nmb/nmb_data/checkpoints2/30000.wav'
 )
 
-fig = plt.figure(figsize = (16, 6))
+gan1 = np.fft.fft(gan1)
+gan1 = abs(gan1)
+gan1_r = np.fft.fftfreq(len(gan1), d = 1.0)
 
-ax1 = fig.add_subplot(2, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2)
-ax3 = fig.add_subplot(2, 2, 3)
-ax4 = fig.add_subplot(2, 2, 4)
+gan2 = np.fft.fft(gan2)
+gan2 = abs(gan2)
+gan2_r = np.fft.fftfreq(len(gan2), d = 1.0)
 
-ax1.set(title = '5000')
-ax2.set(title = '10000')
-ax3.set(title = '20000')
-ax4.set(title = '30000')
+gan3 = np.fft.fft(gan3)
+gan3 = abs(gan3)
+gan3_r = np.fft.fftfreq(len(gan3), d = 1.0)
 
-librosa.display.waveplot(gan1, sr, ax = ax1)
-librosa.display.waveplot(gan2, sr, ax = ax2)
-librosa.display.waveplot(gan3, sr, ax = ax3)
-librosa.display.waveplot(gan4, sr, ax = ax4)
+gan4 = np.fft.fft(gan4)
+gan4 = abs(gan4)
+gan4_r = np.fft.fftfreq(len(gan4), d = 1.0)
 
-fig.tight_layout()
+# fig = plt.figure(figsize = (16, 6))
+
+# ax1 = fig.add_subplot(2, 2, 1)
+# ax2 = fig.add_subplot(2, 2, 2)
+# ax3 = fig.add_subplot(2, 2, 3)
+# ax4 = fig.add_subplot(2, 2, 4)
+
+# ax1.set(title = '5000')
+# ax2.set(title = '10000')
+# ax3.set(title = '20000')
+# ax4.set(title = '30000')
+
+# librosa.display.waveplot(gan1, sr, ax = ax1)
+# librosa.display.waveplot(gan2, sr, ax = ax2)
+# librosa.display.waveplot(gan3, sr, ax = ax3)
+# librosa.display.waveplot(gan4, sr, ax = ax4)
+
+# ax1.plot(gan1_r, gan1)
+# ax2.plot(gan2_r, gan2)
+# ax3.plot(gan3_r, gan3)
+# ax4.plot(gan4_r, gan4)
+
+plt.plot(gan4_r, gan4)
+plt.xlim(0, 0.5)
+plt.ylim(0, 100)
+
+# fig.tight_layout()
+# plt.xlim(0, 5)
+# plt.ylim(0, 100)
 plt.show()
-
 
 ######################### csv visualization ############################
 
