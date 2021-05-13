d3b5127832259897cf02e0fd5a91a85be62b843d
skanwngud <skanwngud@gmail.com>
2021-05-13 09:37:49 +0900

    commit

--- model/ml_all.py
+++ model/ml_all.py
@@ -61,3 +61,47 @@ for (name, algorithm) in allAlgorithms :
 end_now = datetime.datetime.now()
 time = end_now - start_now
 print("time >> " , time)    # time >
+
+'''
+AdaBoostClassifier 의 정답률 :  0.8964757709251101
+BaggingClassifier 의 정답률 :  0.8865638766519823
+BernoulliNB 의 정답률 :  0.5506607929515418
+CalibratedClassifierCV 의 정답률 :  0.9251101321585903
+CategoricalNB 은 없는 모델
+ClassifierChain 은 없는 모델
+ComplementNB 은 없는 모델
+DecisionTreeClassifier 의 정답률 :  0.8381057268722467
+DummyClassifier 의 정답률 :  0.5506607929515418
+ExtraTreeClassifier 의 정답률 :  0.775330396475771
+ExtraTreesClassifier 의 정답률 :  0.9118942731277533
+GaussianNB 의 정답률 :  0.5099118942731278
+GaussianProcessClassifier 의 정답률 :  0.5506607929515418
+GradientBoostingClassifier 의 정답률 :  0.9273127753303965
+HistGradientBoostingClassifier 의 정답률 :  0.9229074889867841
+KNeighborsClassifier 의 정답률 :  0.8370044052863436
+LabelPropagation 의 정답률 :  0.5506607929515418
+LabelSpreading 의 정답률 :  0.5506607929515418
+LinearDiscriminantAnalysis 의 정답률 :  0.9162995594713657
+LinearSVC 의 정답률 :  0.9185022026431718
+LogisticRegression 의 정답률 :  0.9129955947136564
+LogisticRegressionCV 의 정답률 :  0.9185022026431718
+MLPClassifier 의 정답률 :  0.44933920704845814
+MultiOutputClassifier 은 없는 모델
+MultinomialNB 은 없는 모델
+NearestCentroid 의 정답률 :  0.802863436123348
+NuSVC 의 정답률 :  0.9151982378854625
+OneVsOneClassifier 은 없는 모델
+OneVsRestClassifier 은 없는 모델
+OutputCodeClassifier 은 없는 모델
+PassiveAggressiveClassifier 의 정답률 :  0.9251101321585903
+Perceptron 의 정답률 :  0.7775330396475771
+QuadraticDiscriminantAnalysis 의 정답률 :  0.44933920704845814
+RadiusNeighborsClassifier 은 없는 모델
+RandomForestClassifier 의 정답률 :  0.9052863436123348
+RidgeClassifier 의 정답률 :  0.8931718061674009
+RidgeClassifierCV 의 정답률 :  0.8931718061674009
+SGDClassifier 의 정답률 :  0.9107929515418502
+SVC 의 정답률 :  0.9427312775330396
+StackingClassifier 은 없는 모델
+VotingClassifier 은 없는 모델
+'''
