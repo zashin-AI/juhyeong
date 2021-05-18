f92d8decaec65964c149108838da8cd7de5b7007
skanwngud <skanwngud@gmail.com>
2021-05-18 14:26:24 +0900

    commit

--- model/ml_cat_list.py
+++ model/ml_cat_list.py
@@ -1,9 +1,11 @@
 import os
+import pickle
 import numpy as np
 import datetime
 import librosa
 import warnings
 warnings.filterwarnings('ignore')
+import gc
 
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
@@ -22,6 +24,8 @@ y = np.load('c:/nmb/nmb_data/npy/total_label.npy')
 x = x.reshape(-1, x.shape[1] * x.shape[2])
 
 scale_list = [MinMaxScaler(), StandardScaler()]
+# iteration_list = [100, 300, 500, 700, 1000]
+iteration_list = [1, 2, 3, 4, 5]
 audio_path_list = ['c:/nmb/nmb_data/predict/F', 'c:/nmb/nmb_data/predict/M']
 
 count_f = 0
@@ -32,60 +36,72 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 for i in scale_list:
-    scale = i
-    scale.fit(x_train)
-    x_train = scale.transform(x_train)
-    x_test = scale.transform(x_test)
+    for j in iteration_list:
+        scale = i
+        scale.fit(x_train)
+        x_train = scale.transform(x_train)
+        x_test = scale.transform(x_test)
 
-    # for j in model_list:
-    model = CatBoostClassifier(learning_rate = 0.001)
-    model.fit(x_train, y_train)
-
-    y_pred = model.predict(x_test)
-
-    acc = accuracy_score(y_test, y_pred)
-    loss = log_loss(y_test, y_pred)
-
-    print('scaler : \n' + str(i) + 'model : CatBoostClassifier' + 'acc : \n', acc)
-    print('\nloss : \n', loss)
-
-    f = open('c:/nmb/nmb_data/' + str(i) + '_' + 'CatBoostClassifier' + '.txt', 'w')
-    f.write(
-        'scaler : ' + str(i) + '\n' +\
-                'model : CatBoostClassifier' + '\n' +\
-                    'acc : ' + acc + '\n' +\
-                        'loss : ' + loss + '\n\n')
-
-    for pred in audio_path_list:
-        files = librosa.util.find_files(pred, ext = ['wav'])
-        files = np.asarray(file)
-        
-        for file in files:
-            name = os.path.basename(file)
-            length = len(name)
-            name = name[0]
-
-            y, sr = librosa.load(file, sr = 22050)
-            mels = librosa.feature.melspectrogram(
-                y, sr = sr, n_fft = 512, hop_length = 128, win_length = 512
+        # for j in model_list:
+        model = CatBoostClassifier(
+            iterations = j
             )
-            y_mels = librosa.amplitude_to_db(mels ,ref = np.max)
-            y_mels = y_mels.reshape(1, y_mels.shape[0] * y_mels.shape[1])
+        model.fit(x_train, y_train)
 
-            y_mels = scale.transform(y_mels)
+        y_pred = model.predict(x_test)
 
-            y_pred = model.predict(y_pred)
+        acc = accuracy_score(y_test, y_pred)
+        loss = log_loss(y_test, y_pred)
 
-            if y_pred == 0:
-                if name == 'F':
-                    count_f += 1
+        print('scaler : \n' + str(i) + '\nmodel : CatBoostClassifier\n' + 'acc : \n', acc)
+        print('\nloss : \n', loss)
+        pickle.dump(
+            model,
+            open(
+                'c:/data/modelcheckpoint/project_catboost_lr_default_' + str(i) + '_' + str(j) + '.data', 'wb'
+            )
+        )
 
-            elif y_pred == 1:
-                if name == 'M':
-                    count_m += 1
+        f = open('c:/nmb/nmb_data/' + str(i) + '_' + str(j) + '_' + 'CatBoostClassifier' + '.txt', 'w')
+        f.write(
+            'scaler : ' + str(i) + '\n' +\
+                'iteration : ' + str(j) + '\n' + \
+                    'model : CatBoostClassifier' + '\n' +\
+                        'acc : ' + str(acc) + '\n' +\
+                            'loss : ' + str(loss) + '\n\n')
 
-    print('43개 중의 목소리 중 여자는 ' + str(count_f) + '개 입니다.')
-    print('43개 중의 목소리 중 남자는 ' + str(count_m) + '개 입니다.')
-    print('time : ', datetime.datetime.now() - str_time)
-    f.write('43개 중의 목소리 중 여자는 ' + str(count_f) + '개 입니다.\n43개 중의 목소리 중 남자는 ' + str(count_m) + '개 입니다.\n')
-    f.close()
+        for pred in audio_path_list:
+            files = librosa.util.find_files(pred, ext = ['wav'])
+            files = np.asarray(files)
+            
+            for file in files:
+                name = os.path.basename(file)
+                length = len(name)
+                name = name[0]
+
+                y, sr = librosa.load(file, sr = 22050)
+                mels = librosa.feature.melspectrogram(
+                    y, sr = sr, n_fft = 512, hop_length = 128, win_length = 512
+                )
+                y_mels = librosa.amplitude_to_db(mels ,ref = np.max)
+                y_mels = y_mels.reshape(1, y_mels.shape[0] * y_mels.shape[1])
+
+                y_mels = scale.transform(y_mels)
+
+                y_pred = model.predict(y_mels)
+
+                if y_pred == 0:
+                    if name == 'F':
+                        count_f += 1
+
+                elif y_pred == 1:
+                    if name == 'M':
+                        count_m += 1
+
+        print('43개 중의 목소리 중 여자는 ' + str(count_f) + '개 입니다.')
+        print('43개 중의 목소리 중 남자는 ' + str(count_m) + '개 입니다.')
+        print('time : ', datetime.datetime.now() - str_time)
+        f.write('43개 중의 목소리 중 여자는 ' + str(count_f) + '개 입니다.\n43개 중의 목소리 중 남자는 ' + str(count_m) + '개 입니다.\n' + \
+            'time : ' + str(datetime.datetime.now() - str_time))
+        f.close()
+        gc.collect()
